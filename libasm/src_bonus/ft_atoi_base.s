90a27f3cfb0164aa3cb06664ecbdb44a7420c5c2
Blood Marillion <bmarilli@er-c2.kzn.21-school.ru>
2021-02-14 22:35:35 +0300

    add bonus

--- libasm/src_bonus/ft_atoi_base.s
+++ libasm/src_bonus/ft_atoi_base.s
@@ -1,109 +1,181 @@
-global _ft_atoi_base
+section		.text
 
-section	.text
+global		_ft_atoi_base
+
+extern		_ft_strlen
+
+
+
+;------------------------------------------------------------------------------;
+; int  ft_is_space(char c);                                                    ;
+;------------------------------------------------------------------------------;
+
+ft_is_space:
+	cmp		byte [rdi], 0x09
+	je		.is_space
+	cmp		byte [rdi], 0x0A
+	je		.is_space
+	cmp		byte [rdi], 0x0B
+	je		.is_space
+	cmp		byte [rdi], 0x0C
+	je		.is_space
+	cmp		byte [rdi], 0x0D
+	je		.is_space
+	cmp		byte [rdi], 0x20
+	je		.is_space
+.is_not_space:
+	mov		rax, -1
+	ret
+.is_space:
+	mov		rax, 1
+	ret
+
+;------------------------------------------------------------------------------;
+; int  ft_is_sign(char c);                                                     ;
+;------------------------------------------------------------------------------;
+
+ft_is_sign:
+	cmp		byte [rdi], 0x2B
+	je		.is_plus
+	cmp		byte [rdi], 0x2D
+	je		.is_minus
+	mov		rax, 0
+	ret
+.is_plus:
+	mov		rax, 1
+	ret
+.is_minus:
+	mov		rax, -1
+	ret
+
+;------------------------------------------------------------------------------;
+; int  ft_dup_char(char *rest_string_to_compare);                              ;
+;------------------------------------------------------------------------------;
+
+ft_dup_char:
+	mov		r9b, byte[rsi]
+	inc		rsi
+.looping:
+	cmp		byte [rsi], 0
+	je		.end_of_str
+	cmp		byte [rsi], r9b
+	je		.dup_char
+	inc		rsi
+	jmp		.looping
+.dup_char:
+	mov		rax, 1
+	ret
+.end_of_str:
+	mov		rax, 0
+	ret
+
+;------------------------------------------------------------------------------;
+; int  ft_check_base(char *base);                                              ;
+;------------------------------------------------------------------------------;
+
+ft_check_base:
+	cmp		byte[rdi], 0
+	je		.b_error
+	call	_ft_strlen
+	cmp		rax, 1
+	jle		.b_error
+	push	rax
+.loop_check_char:
+	cmp		byte [rdi], 0
+	je		.end_of_base
+	call	ft_is_space
+	cmp		rax, 0
+	jg		.base_error
+	call	ft_is_sign
+	cmp		rax, 0
+	jne		.base_error
+	push	rsi
+	mov		rsi, rdi
+	call	ft_dup_char
+	pop		rsi
+	cmp		rax, 0
+	jg		.base_error
+	inc		rdi
+	jmp		.loop_check_char
+.end_of_base:
+	pop		rax
+	ret
+.base_error:
+	pop		rax
+.b_error:
+	mov		rax, 0
+	ret
+
+;------------------------------------------------------------------------------;
+; int  ft_posit_base(char *str_posit_pointer, char *base);                     ;
+;------------------------------------------------------------------------------;
+
+ft_posit_base:
+	xor		rax, rax
+	mov		cl, [rdi]
+.nextchar:
+	cmp		byte [rsi + rax], 0
+	jz		.not_base
+	cmp		cl, [rsi + rax]
+	je		.posit
+	inc		rax
+	jmp		.nextchar
+.posit:
+	ret
+.not_base:
+	mov		rax, -1
+	ret
 
 ;------------------------------------------------------------------------------;
 ; int  ft_atoi_base(char *str, char *base);                                    ;
 ;------------------------------------------------------------------------------;
 
 _ft_atoi_base:
-														; rdi = *str rsi = *base
-						mov		r8, 1					; sign = 1
-						xor		rax, rax				; res = 0
-						cmp		rsi, 0					; !base
-						je		.return
-						xor		rcx,rcx					; i = 0
-						xor		rbx,rbx					; j = 0 (increment for check repeat)
-						xor		r9, r9					; base_len = 0
-.check_base:
-						cmp		BYTE [rsi + rcx], 0		; base[i] == \0
-						je		.rcx_zero
-						cmp		BYTE [rsi + rcx], 32	; base[i] == ' '
-						je	 	.return
-						cmp		BYTE [rsi + rcx], 9		; base[i] == \t
-						je		.return
-						cmp		BYTE [rsi + rcx], 10	; base[i] == \n
-						je		.return
-						cmp		BYTE [rsi + rcx], 11	; b[i] == \v
-						je		.return
-						cmp		BYTE [rsi + rcx], 12	; b[i] == \f
-						je		.return
-						cmp		BYTE [rsi + rcx], 13	; b[i] == \r
-						je		.return
-						cmp		BYTE [rsi + rcx], 45	; base[i] == '-'
-						je	 	.return
-						cmp		BYTE [rsi + rcx], 43	; base[i] == '+'
-						je		.return
-						xor		rbx,rbx					; j = 0  
-.check_base_repeat:
-						cmp		rbx, r9					; j == base_len
-						je		.check_base_increment
-						mov		dl, [rsi + rbx]
-						cmp		[rsi + rcx], dl			; base[i] == base[j]
-						je		.return
-						inc		rbx						; j++
-						jmp		.check_base_repeat
-.check_base_increment:
-						inc		rcx						; i++
-						inc		r9						; base_len++
-						jmp		.check_base
-.rcx_zero:
-						xor		rcx, rcx				; i = 0
-.isspace:
-						cmp		BYTE [rdi + rcx], 0		; str[i] == \0
-						je		.return
-						cmp		r9, 1					; base_len <= 1
-						jle		.return
-						cmp		BYTE [rdi + rcx], 32	; str[i] == ' '
-						je	 	.space
-						cmp		BYTE [rdi + rcx], 9		; str[i] == \t
-						je		.space
-						cmp		BYTE [rdi + rcx], 10	; str[i] == \n
-						je		.space
-						cmp		BYTE [rdi + rcx], 11	; s[i] == \v
-                        je		.space
-						cmp		BYTE [rdi + rcx], 12	; s[i] == \f
-						je		.space
-						cmp		BYTE [rdi + rcx], 13	; s[i] == \r
-                        je		.space
-                        jmp		.issign
-.space:
-						inc		rcx						; i++
-						jmp		.isspace
-.issign:
-						cmp		BYTE [rdi + rcx], 0		; str[i] == \0
-						je		.return
-						cmp		BYTE [rdi + rcx], 45	; str[i] == '-'
-                        je	 	.negative
-                        cmp		BYTE [rdi + rcx], 43	; str[i] == '+'
-                        je		.positive
-                        jmp		.zero_j
-						
-.negative:				neg		r8						; sign = -1
-.positive:				inc		rcx						; i++
-						jmp		.issign
-.zero_j:
-						xor 	rbx, rbx				; j == 0	
-.is_in_base:
-						cmp		BYTE [rdi + rcx], 0		; str[i] == \0
-						je		.return
-						mov		dl, BYTE [rsi + rbx]
-						cmp 	BYTE [rdi + rcx], dl	; str[i] == base[j]
-						je		.add
-						inc		rbx						; j++
-						cmp		BYTE [rsi + rbx], 0		; str[j] == \0
-						je		.return
-						jmp		.is_in_base
-
-.add:
-						mul		r9						;rax = rax * base len
-						add		rax, rbx				; res += j
-						inc		rcx						; i++
-						jmp		.zero_j
-
-.return:
-						cmp		r8, 1					; sign == 1
-						je		.end
-						neg		rax						; res = - res
-.end:
-						ret
+	cmp		rdi, 0
+	je		.error
+	cmp		rsi, 0
+	je		.error
+	push	rdi				; store str address
+	mov		rdi, rsi		; mov the base addr to the argument
+	call	ft_check_base	; check and return the base_len if it works
+	mov		rbx, rax		; store base_len to some register
+	pop		rdi				; recover str address
+	cmp		rbx, 0			; if base_error, exit function
+	je		.error
+	dec		rdi
+.while_is_space:
+	inc		rdi
+	call	ft_is_space
+	cmp		rax, 0
+	jg		.while_is_space
+	mov		r9, 1
+	mov		rax, 1
+	dec		rdi
+.while_is_sign:
+	mul		r9
+	mov		r9, rax
+	inc		rdi
+	call	ft_is_sign
+	cmp		rax, 0
+	jne		.while_is_sign
+	call	ft_posit_base	; return the posit of the char on the base string
+	cmp		rax, 0			; check if (char) belongs to the base
+	jl		.error
+	inc		rdi;
+.loop_is_base:
+	push	rax;			; store posit
+	call	ft_posit_base
+	mov		rcx, rax		; save new posit into other register
+	pop		rax				; recover posit
+	cmp		rcx, 0			; check if (char) belongs to the base
+	jl		.exit
+	mul		rbx				; rax = rax * rbx, rbx is base_size
+	add		rax, rcx		; add the new posit
+	inc		rdi				; increment string  address
+	jmp		.loop_is_base
+.exit:
+	mul		r9
+	ret
+.error:
+	mov		rax, 0
+	ret
--- libasm/src_bonus/ft_list_remove_if.s
+++ libasm/src_bonus/ft_list_remove_if.s
@@ -1,69 +1,73 @@
-global		_ft_list_remove_if
-
 section		.text
+
+global	_ft_list_remove_if
 
 ;------------------------------------------------------------------------------;
 ; void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(),    ;
 ;       void (*free_fct)(void *))                                              ;
 ;------------------------------------------------------------------------------;
 
-_ft_list_remove_if:
-	mov		r8, rdi				; begin_list
-	mov		r13, rsi			; data ref
-	cmp		r8, 0				; check if pointer is null
-	je		.exit
-	cmp		qword[r8], 0		; check if list is null
-	je		.exit
-	mov		r9, [r8]			; ptr
-.looping:
-	cmp		qword[r8 + 8], 0	; check end of list
-	je		.exit
-	cmp		r9, 0
-	je		.exit
-	mov		rdi, [r9]			; ptr->data
-	mov		rsi, r13
-	push	rdx
-	push	rcx
-	call	rdx					; call *cmp() function
-	pop		rcx
-	pop		rdx
-	je		.remove_node
-	mov		r8, r9
-	mov		r9, [r9 + 8]		; iterate pointers
-	jmp		.looping
-.remove_node:
-	mov		r10, [r9]
-	push	r10					; store the node addr to free
-	mov		rdi, r9
-	cmp		[r8], r9
-	je		.first_node
-	mov		r9, [r9 + 8]
-	mov		[r8 + 8], r9
-	jmp		.free_node
-.first_node:
-	mov		r12, [r9 + 8]
-	cmp		r12, 0
-	je		.only_node
-	mov		[r8], r12
-	mov		r9, [r9 + 8]
-	jmp		.free_node
-.only_node:
-	mov		qword[r8], 0
-	mov		r9, 0
-	jmp		.free_node
-.free_node:
-	push	rdx
-	push	rcx					; free the data
-	call	rcx					; call *free_fct() function
-	pop		rcx
-	pop		rdx
-	pop		r10
-	mov		rdi, r10			; move the elem removed to the argument
-	push	rdx
-	push	rcx					; free the node
-	call	rcx					; call *free_fct() function
-	pop		rcx
-	pop		rdx
-	jmp		.looping
-.exit:
-	ret
+_ft_list_remove_if:		; rdi = **list rsi = *data_ref rdx = *cmp rcx = *free_fnc
+						push	rsp
+						cmp		rdi, 0				; !**list
+						je		.return
+						cmp		qword [rdi], 0		; !*list
+						je		.return
+						mov		r8, rdi				; r8 = **begin
+						mov		r9, [rdi]			; r9 = *list
+						xor		r10, r10			; r10 = *previous
+.loop:
+						cmp		r9, 0				; !*list
+						je		.return
+						mov		rdi, [r9]			; rdi = list->data
+						push	rdx
+						push	rcx
+						push	rsi
+						push	r8
+						push	r9
+						push	r10
+						call	rdx					; *cmp(list->data, data_ref)
+						pop		r10
+						pop		r9
+						pop		r8
+						pop		rsi
+						pop		rcx
+						pop		rdx
+						cmp		eax, 0
+						je		.delete_elem
+						mov		r10, r9				; r10 = *list
+						mov		r9, [r9 + 8]		; r9 = *next
+						jmp		.loop
+
+.delete_elem:
+						cmp		r10, 0				; *begin == *list
+						je		.first_elem
+						mov		rax, [r9 + 8]		; r9 = list->next
+						mov		[r10 + 8], rax		; prev->next = list->next
+
+.free:
+						mov		rdi, r9				;
+						mov		r9, [r9 + 8]
+						push	rdx
+						push	rcx
+						push	rsi
+						push	r8
+						push	r9
+						push	r10
+						call	rcx					; free_fct(list->data)
+						pop		r10
+						pop		r9
+						pop		r8
+						pop		rsi
+						pop		rcx
+						pop		rdx
+						jmp		.loop
+
+.first_elem:
+						mov		rax, [r9 + 8]
+						mov		[r8], rax			; *begin = list->next
+						jmp		.free
+
+.return:
+						pop		rsp
+						ret
